654d222f9b84d99fbfda84d5fe367a5693aacd9a
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2025-11-25 16:52:44 -0300

    ajuste item-task

---
+++ docs/ref.md
@@ -0,0 +1,2 @@
+* link último visto
+https://alunos.fullstackclub.com.br/area/produto/item/2834907
--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,8 +1,25 @@
+import "./TaskItem.scss"
+import { AiFillDelete } from "react-icons/ai"
+
 const TaskItem = ({ task } ) => {
     return (
         <>
-            <h1> {task.description} </h1>
-            <p> { task.isCompleted ? "Completa" : "Não completa" } </p>
+        <div className="task-item-container">
+            <div className="task-description">
+                <label className={ task.isCompleted ? 'checkbox-container-completed' : 'checkbox-container'}>
+                    <p>{task.description}</p>
+                    <input type="checkbox" id={task.id}
+                        defaultChecked={task.isCompleted }
+                    />
+                    <span className={task.isCompleted ? 'checkmark completed' : 'checkmark'} > 
+                        {/* { task.isCompleted ? "Completa" : "Não completa" } */}
+                    </span>
+                </label>    
+            </div>
+            <div className="delete">
+                <AiFillDelete size={18} color="red" />
+            </div>
+        </div>
         </>
     );
 }
